e2c05d5de5a6098c4f07f2a4afc1551e473b1ef7
James Feore <jjfeore@gmail.com>
2017-04-20 10:27:37 -0700

    Done with comment answers

--- Christina-James/server.js
+++ Christina-James/server.js
@@ -24,7 +24,7 @@ app.get('/new', function(request, response) {
 });
 
 app.get('/articles', function(request, response) {
-  // Answer: This uses parts 1-4. It is interacted with by Article.fetchAll(). This just READs.
+  // Answer: This uses parts 2-5. It is interacted with by Article.fetchAll(). This just READs.
   client.query('SELECT * FROM articles')
   .then(function(result) {
     response.send(result.rows);
@@ -35,7 +35,7 @@ app.get('/articles', function(request, response) {
 });
 
 app.post('/articles', function(request, response) {
-  // ANSWER: This uses parts 1-4 of the diagram. This works with Article.prototype.insertRecord(). This performs the CREATE part of the CRUD.
+  // ANSWER: This uses parts 2-5 of the diagram. This works with Article.prototype.insertRecord(). This performs the CREATE part of the CRUD.
   client.query(
     `INSERT INTO
     articles(title, author, authorUrl, category, publishedOn, body)
@@ -59,7 +59,7 @@ app.post('/articles', function(request, response) {
 });
 
 app.put('/articles/:id', function(request, response) {
-  // ANSWER: This uses parts 1-4. This is used by Article.prototype.updateRecord(). It does the UPDATE part of the CRUD.
+  // ANSWER: This uses parts 2-5. This is used by Article.prototype.updateRecord(). It does the UPDATE part of the CRUD.
   client.query(
     `UPDATE articles
     SET
@@ -85,7 +85,7 @@ app.put('/articles/:id', function(request, response) {
 });
 
 app.delete('/articles/:id', function(request, response) {
-  // ANSWER: This uses parts 1-4. This is used by Article.prototype.deleteRecord(). It does the DELETE part of the CRUD.
+  // ANSWER: This uses parts 2-5. This is used by Article.prototype.deleteRecord(). It does the DELETE part of the CRUD.
   client.query(
     `DELETE FROM articles WHERE article_id=$1;`,
     [request.params.id]
@@ -99,7 +99,7 @@ app.delete('/articles/:id', function(request, response) {
 });
 
 app.delete('/articles', function(request, response) {
-  // ANSWER: This uses parts 1-4. This is used by Article.prototype.deleteRecord(). It does the DELETE part of the CRUD, on the ENTIRE TABLE.
+  // ANSWER: This uses parts 2-5. This is used by Article.prototype.deleteRecord(). It does the DELETE part of the CRUD, on the ENTIRE TABLE.
   client.query(
     'DELETE FROM articles;'
   )
@@ -111,27 +111,17 @@ app.delete('/articles', function(request, response) {
   });
 });
 
-// COMMENT: What is this function invocation doing?
-// Put your response here...
+// ANSWER: As soon as loadDB() rubns, it creates a blank new table (if one doesn't exist yet) called articles. If articles has no rows (which it shouldn't), it then populates articles with all the stuff in hackerIpsum.json
 loadDB();
 
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}!`);
 });
 
-
-//////// ** DATABASE LOADER ** ////////
-////////////////////////////////////////
 function loadArticles() {
-  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // Put your response here...
+  // ANSWER: This uses parts 3-4. It isn't used in article.js, and performs the CREATE part of the CRUD.
   client.query('SELECT COUNT(*) FROM articles')
   .then(result => {
-    // REVIEW: result.rows is an array of objects that Postgres returns as a response to a query.
-    //         If there is nothing on the table, then result.rows[0] will be undefined, which will
-    //         make count undefined. parseInt(undefined) returns NaN. !NaN evaluates to true.
-    //         Therefore, if there is nothing on the table, line 151 will evaluate to true and
-    //         enter into the code block.
     if(!parseInt(result.rows[0].count)) {
       fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
         JSON.parse(fd.toString()).forEach(ele => {
@@ -149,16 +139,15 @@ function loadArticles() {
 }
 
 function loadDB() {
-  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // Put your response here...
+  // ANSWER: This uses parts 3-4. It isn't used in article.js, and performs the CREATE part of the CRUD.
   client.query(`
     CREATE TABLE IF NOT EXISTS articles (
       article_id SERIAL PRIMARY KEY,
       title VARCHAR(255) NOT NULL,
       author VARCHAR(255) NOT NULL,
-      "authorUrl" VARCHAR (255),
+      authorUrl VARCHAR (255),
       category VARCHAR(20),
-      "publishedOn" DATE,
+      publishedOn DATE,
       body TEXT NOT NULL);`
     )
     .then(function() {
